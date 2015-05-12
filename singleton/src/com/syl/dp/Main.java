f6756e9417d016dbfb532a179a4bd74d19e43b78
sylcrq <syl272365943@126.com>
2015-05-12 16:16:41 +0800

    +):添加单例模式实现

---
+++ singleton/src/com/syl/dp/Main.java
@@ -0,0 +1,13 @@
+package com.syl.dp;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Singleton obj = Singleton.getInstance();
+		obj.doSomething();
+	}
+
+}
---
+++ singleton/src/com/syl/dp/Singleton.java
@@ -0,0 +1,32 @@
+package com.syl.dp;
+
+/**
+ * 一个简单的单例模式实现(Not Thread Safe)
+ * @author shenyunlong
+ *
+ * Create by syl, 2015/05/12
+ */
+
+public class Singleton {
+	// private 单例
+	private static Singleton mSingletonObj;
+	
+	// private 构造函数
+	private Singleton() {
+		System.out.println("Singleton Constructor");
+	}
+	
+	// 全局访问接口
+	public static Singleton getInstance() {
+		// 延迟实例化
+		if(mSingletonObj == null) {
+			mSingletonObj = new Singleton();
+		}
+		
+		return mSingletonObj;
+	}
+	
+	public void doSomething() {
+		System.out.println("Do Something");
+	}
+}
